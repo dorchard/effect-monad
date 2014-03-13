fa33ddceb8a06e501e5f23dd4e53b2b965ba3868
Dominic Orchard <dom.orchard@gmail.com>
2014-03-13 16:26:04 +0000

    updated to HList >= 0.3

--- src/Control/IxMonad/Reader.hs
+++ src/Control/IxMonad/Reader.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE TypeFamilies, MultiParamTypeClasses, FlexibleInstances #-}
 
-module Control.IxMonad.Reader (HNil(..), HCons(..), ask, Split(..)) where
+module Control.IxMonad.Reader (HNil'(..), HCons'(..), ask, Split(..)) where
 
 import Control.IxMonad
 import Control.IxMonad.Cond
@@ -10,15 +10,15 @@ import Prelude    hiding (Monad(..))
 instance IxMonad (->) where
     type Inv (->) s t = Split s t
 
-    type Unit (->) = HNil
+    type Unit (->) = HNil'
     type Plus (->) s t = Append s t
 
-    return x = \HNil -> x
+    return x = \HNil' -> x
     e >>= k = \xs -> let (s, t) = split xs
                      in (k (e s)) t
 
-ask :: (HCons a HNil) -> a
-ask = \(HCons a HNil) -> a
+ask :: (HCons' a HNil') -> a
+ask = \(HCons' a HNil') -> a
 
 -- Type-level append, and dual operations for split
 
@@ -26,14 +26,14 @@ class Split s t where
     type Append s t
     split :: Append s t -> (s, t)
 
-instance Split HNil t where
-    type Append HNil t = t
-    split t = (HNil, t)
+instance Split HNil' t where
+    type Append HNil' t = t
+    split t = (HNil', t)
 
-instance Split xs ys => Split (HCons x xs) ys where
-    type Append (HCons x xs) ys = HCons x (Append xs ys)
-    split (HCons x xs) = let (xs', ys') = split xs
-                         in (HCons x xs', ys')
+instance Split xs ys => Split (HCons' x xs) ys where
+    type Append (HCons' x xs) ys = HCons' x (Append xs ys)
+    split (HCons' x xs) = let (xs', ys') = split xs
+                         in (HCons' x xs', ys')
 
 -- Conditionals
 
