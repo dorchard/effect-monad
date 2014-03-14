4820730c0d2b6f72ed7dc55beccdf8fd480de826
Dominic Orchard <dom.orchard@gmail.com>
2014-03-14 14:48:43 +0000

    sized list with multiplication monoid on size indices

---
+++ src/Control/IxMonad/Vector.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE GADTs, TypeFamilies, EmptyDataDecls, UndecidableInstances, MultiParamTypeClasses, TypeSynonymInstances, FlexibleContexts #-}
+
+module Control.IxMonad.Vector where
+
+import Prelude hiding (Monad(..))
+import Control.IxMonad
+
+-- Sized-vector type
+
+data Z
+data S n 
+
+data Vector n a where
+    Nil :: Vector Z a
+    Cons ::a -> Vector n a -> Vector (S n) a
+
+-- Append function which adds indices
+
+type family Add s t
+type instance Add Z     m = m
+type instance Add (S n) m = S (Add n m)
+
+append :: Vector n a -> Vector m a -> Vector (Add n m) a
+append Nil         ys = ys
+append (Cons x xs) ys = Cons x (append xs ys)
+
+instance IxMonad Vector where
+    -- Effect monoid is (N, *, 1)
+
+    type Unit Vector = S Z
+
+    -- Multiplies indicies
+    type Plus Vector Z     m = Z
+    type Plus Vector (S n) m = Add m (Plus Vector n m)
+
+    return x = Cons x Nil
+    Nil         >>= f  = Nil
+    (Cons x xs) >>= f  = append (f x) (xs >>= f)
+
+
+
