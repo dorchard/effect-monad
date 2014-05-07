e4fdf412998ab64d44b6191cc273c6e8a31885c6
Dominic <dom.orchard@gmail.com>
2014-05-08 00:49:56 +0100

    updates

--- examples/StateProblem1.hs
+++ examples/StateProblem1.hs
@@ -4,11 +4,13 @@ import Control.Monad.State.Lazy
 
 -- inc :: (MonadState s m, Num s) => m ()
 
+inc :: (Monad m, Num s) => StateT s m ()
 inc = do x <- get
          put (x + 1)
 
 -- append :: (MonadState [a] m) => [a] -> m ()
 
+append :: (Monad m) => [a] -> StateT [a] m ()
 append y = do x <- get
               put (x ++ y)
 
@@ -16,11 +18,11 @@ append y = do x <- get
 
 -- need to add lifts, must make some arbitrary choice
 
-foo = do inc
-         lift $ append "world"
+prog1 = do inc
+           lift $ append "world"
 
-runFoo = runStateT (runStateT foo 41) "hello " -- need to get the order right 
-                                               -- but types will help us 
+runProg1 = runStateT (runStateT prog1 41) "hello " -- need to get the order right 
+                                                 -- but types will help us 
          
 neg = do x <- get
          put (not x)
@@ -32,6 +34,11 @@ foo2 = do lift $ neg
           
 runFoo2 = runStateT (runStateT (runStateT foo2 41) False) "hello "
 
+prog2 = do inc
+           lift $ append "world"
+           lift $ lift $ append "Nice to"
+
+runProg2 = runStateT (runStateT (runStateT prog2 41) "Hello ") " meet you"
 
 foo3 = do lift $ neg
           inc
--- src/Control/IxMonad/Reader.hs
+++ src/Control/IxMonad/Reader.hs
@@ -11,12 +11,12 @@ import Data.Proxy
 
 -- Type-level list
 
-data Nil
+data Empty
 data Cons (k :: Symbol) (v :: *) (xs :: *)
 
-data List n where
-    Nil :: List Nil
-    Cons :: Proxy (k :: Symbol) -> v -> List xs -> List (Cons k v xs)
+data Set n where
+    Empty :: Set Empty
+    Cons :: Proxy (k :: Symbol) -> v -> Set xs -> Set (Cons k v xs)
 
 -- Type-level set union
 --    implemented using lists, with a canonical ordering and duplicates removed
@@ -24,20 +24,20 @@ type family Union s t where Union s t = RemDup (Bubble (Append' s t))
 
 -- Type-level list append
 type family Append' s t where
-       Append' Nil t = t
+       Append' Empty t = t
        Append' (Cons k x xs) ys = Cons k x (Append' xs ys)
 
 -- Remove duplicates from a type-level list
 type family RemDup t where
-            RemDup Nil                      = Nil
-            RemDup (Cons k a  Nil)          = Cons k a Nil
+            RemDup Empty                      = Empty
+            RemDup (Cons k a  Empty)          = Cons k a Empty
             RemDup (Cons k a (Cons k a as)) = Cons k a (RemDup as)
             RemDup (Cons k a (Cons j b as)) = Cons k a (Cons j b (RemDup as))
 
 -- Type-level bubble sort on list
 type family Bubble l where
-            Bubble Nil                     = Nil
-            Bubble (Cons k a Nil)          = Cons k a Nil
+            Bubble Empty                     = Empty
+            Bubble (Cons k a Empty)          = Cons k a Empty
             Bubble (Cons j a (Cons k b c)) = Cons (MinKey j k j k) (MinKey j k a b) 
                                                (Bubble (Cons (MaxKey j k j k) (MaxKey j k a b) c))
 
@@ -52,41 +52,41 @@ type family Choose (g :: Ordering) a b where
 
 -- Indexed reader type
 
-data IxReader s a = IxR { unIxR :: List s -> a }
+data IxReader s a = IxR { unIxR :: Set s -> a }
 
 -- Indexed monad instance
 
 instance IxMonad IxReader where
     type Inv IxReader s t = Split s t (Union s t)
 
-    type Unit IxReader = Nil
+    type Unit IxReader = Empty
     type Plus IxReader s t = Union s t
 
-    return x = IxR $ \Nil -> x
+    return x = IxR $ \Empty -> x
     (IxR e) >>= k = IxR $ \xs -> let (s, t) = split xs
                                  in (unIxR $ k (e s)) t
 
 -- 'ask' monadic primitive
 
-ask :: Proxy (k::Symbol) -> IxReader (Cons k a Nil) a
-ask Proxy = IxR $ \(Cons Proxy a Nil) -> a
+ask :: Proxy (k::Symbol) -> IxReader (Cons k a Empty) a
+ask Proxy = IxR $ \(Cons Proxy a Empty) -> a
 
 -- Split operation (with type level version)
 
 class Split s t z where
-   split :: List z -> (List s, List t)
+   split :: Set z -> (Set s, Set t)
 
-instance Split Nil Nil Nil where
-   split Nil = (Nil, Nil) 
+instance Split Empty Empty Empty where
+   split Empty = (Empty, Empty) 
 
-instance Split (Cons k x xs) Nil (Cons k x xs) where
-    split t = (t, Nil)
+instance Split (Cons k x xs) Empty (Cons k x xs) where
+    split t = (t, Empty)
 
-instance Split Nil (Cons k x xs) (Cons k x xs) where
-   split t = (Nil, t)
+instance Split Empty (Cons k x xs) (Cons k x xs) where
+   split t = (Empty, t)
 
-instance Split (Cons k x Nil) (Cons k x Nil) (Cons k x Nil) where
-   split (Cons Proxy x Nil) = (Cons Proxy x Nil, Cons Proxy x Nil)
+instance Split (Cons k x Empty) (Cons k x Empty) (Cons k x Empty) where
+   split (Cons Proxy x Empty) = (Cons Proxy x Empty, Cons Proxy x Empty)
 
 instance Split xs ys zs => Split (Cons k x xs) (Cons k x ys) (Cons k x zs) where
    split (Cons k x zs) = let (xs', ys') = split zs
@@ -100,7 +100,7 @@ instance (Split xs ys zs) => Split (Cons j x xs) (Cons k y ys) (Cons k y (Cons j
    split (Cons j x (Cons k y zs)) = let (xs', ys') = split zs
                                     in (Cons k y xs', Cons j x ys')
 
-foo :: IxReader (Cons "x" a (Cons "xs" [a] Nil)) [a]
+foo :: IxReader (Cons "x" a (Cons "xs" [a] Empty)) [a]
 foo = do x <- ask (Proxy::(Proxy "x"))
          xs <- ask (Proxy::(Proxy "xs"))
          return (x:xs)
