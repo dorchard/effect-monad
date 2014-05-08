67ae659e4962c0538e839f6f7cc0cf069d24b319
Dominic <dom.orchard@gmail.com>
2014-05-08 22:40:34 +0100

    some simplifications to sets

--- src/Control/IxMonad/Helpers/Set.hs
+++ src/Control/IxMonad/Helpers/Set.hs
@@ -18,6 +18,56 @@ data Set (n :: [*]) where
     Empty :: Set '[]
     Ext :: Var k -> v -> Set s -> Set ((k :-> v) ': s)
 
+-- ****** Type level *******
+
+-- Set disjoint union (essentially list append)
+
+type family DisjUnion (s :: [*]) (t :: [*]) :: [*]
+type instance DisjUnion '[] t = t
+type instance DisjUnion (x ': xs) ys = x ': (DisjUnion xs ys)
+
+type family RemDups t where
+    RemDups '[]                 = '[]
+    RemDups '[k :-> v]          = '[k :-> v]
+    RemDups ((k :-> u) ': (k :-> u) ': s) = RemDups ((k :-> u) ':s)
+    RemDups ((k :-> v) ': (j :-> u) ': s) = (k :-> v) ': (RemDups ((j :-> u) ': s))
+
+type Union s t = RemDups (Sort (DisjUnion s t))
+
+type Unionable s t = (Sortable (DisjUnion s t), RemDuper (Sort (DisjUnion s t)) (RemDups (Sort (DisjUnion s t))))
+
+-- Sorting for normalising the representation
+
+type Sortable s = Bubbler s s
+
+type Sort l = Bubble l l
+
+type family Bubble l l' where
+    Bubble l '[] = l
+    Bubble l (x ': xs) = Pass (Bubble l xs)
+
+-- Type-level bubble sort on list
+type family Pass (l :: [*]) :: [*]
+type instance Pass '[]                 = '[]
+type instance Pass '[k :-> v]          = '[k :-> v]
+type instance Pass ((j :-> u) ': ((k :-> v) ': s)) = 
+                       ((MinBy j k j k) :-> (MinBy j k u v)) ': 
+                           Pass (((MaxBy j k j k) :-> (MaxBy j k u v)) ': s)
+
+
+-- Return the minimum or maximum of two types which consistitue key-value pairs
+type MinBy (a :: Symbol) (b :: Symbol) (p :: k) (q :: k) = Choose (CmpSymbol a b) p q
+type MaxBy (a :: Symbol) (b :: Symbol) (p :: k) (q :: k) = Choose (CmpSymbol a b) q p
+
+type family Choose (g :: Ordering) a b where
+    Choose LT p q = p
+    Choose EQ p q = p
+    Choose GT p q = q
+
+-- ******* Value level *********
+
+-- Showing for debugging
+
 instance Show (Set '[]) where
     show Empty = "{}"
 
@@ -34,28 +84,16 @@ instance Show' (Set '[]) where
 instance (Show' (Set s), Show (Var k), Show v) => Show' (Set ((k :-> v) ': s)) where
     show' (Ext k v s) = ", (" ++ show k ++ ", " ++ show v ++ ")" ++ (show' s) 
 
--- Type-level set disjoint-union 
 
-type family DisjUnion (s :: [*]) (t :: [*]) :: [*]
-type instance DisjUnion '[] t = t
-type instance DisjUnion ((k :-> v) ': xs) ys = (k :-> v) ': (DisjUnion xs ys)
 
--- Value-level set disjoint union
+-- Set disjoint union
 
 disjunion :: Set s -> Set t -> Set (DisjUnion s t)
 disjunion Empty x = x
 disjunion (Ext k v xs) ys = Ext k v (disjunion xs ys)
 
 
--- Type and value-level removing of duplicates from the set representation
-
-type family RemDups t where
-    RemDups '[]                 = '[]
-    RemDups '[k :-> v]          = '[k :-> v]
-    RemDups ((k :-> u) ': (k :-> u) ': s) = RemDups ((k :-> u) ':s)
-    RemDups ((k :-> v) ': (j :-> u) ': s) = (k :-> v) ': (RemDups ((j :-> u) ': s))
-
-type RemoveDuplicates t = RemDuper t (RemDups t)
+-- Removing of duplicates from the set representation
 
 class RemDuper t v where
     remDup :: Set t -> Set v
@@ -72,33 +110,10 @@ instance (Monoid u, RemDuper ((k :-> u) ': s) s') => RemDuper ((k :-> u) ': (k :
 instance RemDuper ((j :-> u) ': s) s' => RemDuper ((k :-> v) ': (j :-> u) ': s) ((k :-> v) ': s') where
     remDup (Ext k v (Ext j u s)) = Ext k v (remDup (Ext j u s))
 
--- Type- and value-level set union
+-- Set union
 
-type Union s t = RemDups (BSort (DisjUnion s t))
-
-type Unionable s t = (Sortable (DisjUnion s t), RemoveDuplicates (BSort (DisjUnion s t)))
-
-union :: (Sortable (DisjUnion s t), RemoveDuplicates (BSort (DisjUnion s t))) => 
-         Set s -> Set t -> Set (Union s t)
+union :: (Unionable s t) => Set s -> Set t -> Set (Union s t)
 union s t = remDup (bsort (disjunion s t))
-
--- Bubble sort for normalising the representation
-
-type BSort l = Bubble l l
-
-type family Bubble l l' where
-    Bubble l '[] = l
-    Bubble l (a ': y) = BubbleOne (Bubble l y)
-
--- Type-level bubble sort on list
-type family BubbleOne l where
-            BubbleOne '[]                 = '[]
-            BubbleOne '[k :-> v]          = '[k :-> v]
-            BubbleOne ((j :-> u) ': ((k :-> v) ': s)) = 
-                       ((MinKey j k j k) :-> (MinKey j k u v)) ': 
-                           (BubbleOne (((MaxKey j k j k) :-> (MaxKey j k u v)) ': s))
-
-type Sortable s = Bubbler s s
 
 class Bubbler s s' where
     bubble :: Set s -> Set s' -> Set (Bubble s s')
@@ -109,11 +124,11 @@ instance Bubbler s '[] where
 instance (Bubbler s t, Bubbler1 (Bubble s t)) => Bubbler s ((k :-> v) ': t) where
     bubble s (Ext _ _ t) = bubble1 (bubble s t)
 
-bsort :: (Bubbler s s) => Set s -> Set (BSort s)
+bsort :: (Bubbler s s) => Set s -> Set (Sort s)
 bsort x = bubble x x
 
 class Bubbler1 s where
-    bubble1 :: Set s -> Set (BubbleOne s)
+    bubble1 :: Set s -> Set (Pass s)
 
 instance Bubbler1 '[] where
     bubble1 Empty = Empty
@@ -121,26 +136,23 @@ instance Bubbler1 '[] where
 instance Bubbler1 '[(k :-> v)] where
     bubble1 (Ext k v Empty) = Ext k v Empty
 
-instance (Bubbler1 (((MaxKey j k j k) :-> (MaxKey j k u v)) ': s), Chooser (CmpSymbol j k))=>
+instance (Bubbler1 (((MaxBy j k j k) :-> (MaxBy j k u v)) ': s), Chooser (CmpSymbol j k))=>
              Bubbler1 ((j :-> u) ': (k :-> v) ': s) where 
 
     bubble1 (Ext _ u (Ext _ v s)) = Ext Var (minkey (Var::(Var j)) (Var::(Var k)) u v)
-                                         (bubble1 (Ext (Var::(Var (MaxKey j k j k))) (maxkey (Var::(Var j)) (Var::(Var k)) u v) s))
+                                         (bubble1 (Ext (Var::(Var (MaxBy j k j k))) (maxkey (Var::(Var j)) (Var::(Var k)) u v) s))
 
 
 minkey :: forall j k a b . 
           (Chooser (CmpSymbol j k)) => 
-          Var j -> Var k -> a -> b -> MinKey j k a b
+          Var j -> Var k -> a -> b -> MinBy j k a b
 minkey _ _ x y = choose (Proxy::(Proxy (CmpSymbol j k))) x y 
 
 maxkey :: forall j k a b . 
           (Chooser (CmpSymbol j k)) => 
-          Var j -> Var k -> a -> b -> MaxKey j k a b 
+          Var j -> Var k -> a -> b -> MaxBy j k a b 
 maxkey _ _ a b = choose (Proxy::(Proxy (CmpSymbol j k))) b a
 
--- Return the minimum or maximum of two types which consistitue key-value pairs
-type MinKey (a :: Symbol) (b :: Symbol) (p :: k) (q :: k) = Choose (CmpSymbol a b) p q
-type MaxKey (a :: Symbol) (b :: Symbol) (p :: k) (q :: k) = Choose (CmpSymbol a b) q p
 
 class Chooser (o :: Ordering) where
     choose :: (Proxy o) -> p -> q -> (Choose o p q)
@@ -151,7 +163,3 @@ instance Chooser EQ where
 instance Chooser GT where
     choose _ p q = q
 
-type family Choose (g :: Ordering) a b where
-    Choose LT p q = p
-    Choose EQ p q = p
-    Choose GT p q = q
