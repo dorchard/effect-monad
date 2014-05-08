221807b897c538593c6becb417f3d1f288d76673
Dominic Orchard <dom.orchard@gmail.com>
2014-05-08 16:29:42 +0100

    monoid behaviour on writer

--- examples/Writer.hs
+++ examples/Writer.hs
@@ -3,10 +3,21 @@
 import Control.IxMonad
 import Control.IxMonad.Writer
 
+import Data.Monoid
+
 import Prelude hiding (Monad(..))
 
+instance Monoid Int where
+    mappend = (+)
+    mempty  = 0
+
 test :: Writer '["x" :-> Int, "y" :-> String, "z" :-> Bool] ()
-test = do put (Proxy::(Proxy "x")) 42
+test = do -- ...
+          put (Proxy::(Proxy "x")) (42::Int)
+          -- ...
           put (Proxy::(Proxy "y")) "hello"
+          -- ....
           put (Proxy::(Proxy "z")) True 
+          -- .....
+          put (Proxy::(Proxy "x")) (58::Int) -- update to 'x'
 
