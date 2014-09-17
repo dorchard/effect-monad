f4daec89578158411208890654cd74733b9fd15a
Dominic Orchard <dom.orchard@gmail.com>
2014-09-17 16:14:42 +0100

    tweak to state example: more things type check with NoMonomorphismRestriction- unsuprisingly

--- examples/State.hs
+++ examples/State.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DataKinds, RebindableSyntax, TypeOperators, FlexibleInstances #-}
+{-# LANGUAGE DataKinds, RebindableSyntax, TypeOperators, FlexibleInstances, NoMonomorphismRestriction #-}
 
 import Prelude hiding (Monad(..))
 import Control.Effect
@@ -6,6 +6,7 @@ import Control.Effect.State
 
 x_var = Var::(Var "x")
 y_var = Var::(Var "y")
+
 
 {- Computation with a read effect on variable "x" and a read-write (update) effect on variable "y" -}
 foo :: State '["x" :-> Int :! R, "y" :-> [Int] :! RW] [Int]
@@ -17,10 +18,12 @@ foo = do x <- get x_var
 
 foo_run = runState foo (Ext (x_var :-> (1 :! Eff)) (Ext (y_var :-> ([2,3] :! Eff)) Empty))
 
+
 foo2 :: State '["x" :-> Int :! RW] Int
 foo2 = do x <- get x_var
           put x_var (x+1)
           return x
+
 
 foo2_run = (runState foo2) (Ext (x_var :-> 10 :! Eff) Empty)
 
