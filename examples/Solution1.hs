ba6c3a886d6af932970ccbab6f18aa5494974bbb
Dominic Orchard <dom.orchard@gmail.com>
2014-10-09 13:50:18 +0100

    Update Solution1.hs
    tweaked the names in example

--- examples/Solution1.hs
+++ examples/Solution1.hs
@@ -5,23 +5,23 @@ import Prelude hiding (Monad(..))
 import Control.Effect
 import Control.Effect.State
 
-c1_var = Var::(Var "c1")
-c2_var = Var::(Var "c2")
-o_var  = Var::(Var "out")
+c1 = Var::(Var "c1")
+c2 = Var::(Var "c2")
+out  = Var::(Var "out")
 
 incC :: Var v -> State '[v :-> Int :! RW] ()
 incC var = do x <- get var
               put var (x + 1)
 
 writeS :: [a] -> State '["out" :-> [a] :! RW] ()
-writeS y = do x <- get o_var
-              put o_var (x ++ y)
+writeS y = do x <- get out
+              put out (x ++ y)
 
 write ::  [a] -> State '["c1" :-> Int :! RW, "out" :-> [a] :! RW] ()
 write x = do  writeS x
-              incC c1_var
+              incC c1
 
-initState0 = Ext (c1_var :-> ((0::Int) :! Eff)) (Ext (o_var :-> ("" :! Eff)) Empty)
+initState0 = Ext (c1 :-> ((0::Int) :! Eff)) (Ext (out :-> ("" :! Eff)) Empty)
 runWrite = runState (write "hello") initState0
 
 hellow :: State '["c1" :-> Int :! RW, "nom" :-> String :! R, "out" :-> String :! RW] () 
@@ -40,13 +40,13 @@ hello :: State '["buff" :-> String :! RW, "name" :-> String :! R] ()
 hello = do name <- get (Var::(Var "name")) 
            appendBuffer $ "hello " ++ name
 
-initState = Ext (c1_var :-> ((0::Int) :! Eff)) (Ext (o_var :-> ("" :! Eff)) Empty)
+initState = Ext (c1 :-> ((0::Int) :! Eff)) (Ext (out :-> ("" :! Eff)) Empty)
 runHellow = runState hellow initState
 
 hellowCount = do hellow
-                 incC c2_var
+                 incC c2
 
-initState' = Ext (c1_var :-> ((0::Int) :! Eff)) (Ext (c2_var :-> ((1::Int) :! Eff)) (Ext (o_var :-> ("" :! Eff)) Empty))
+initState' = Ext (c1 :-> ((0::Int) :! Eff)) (Ext (c2 :-> ((1::Int) :! Eff)) (Ext (out :-> ("" :! Eff)) Empty))
 runHellowCount = runState hellowCount initState'
 
 -- Show instances
