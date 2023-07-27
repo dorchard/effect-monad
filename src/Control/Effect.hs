d4d995e7fd52476cf1d8c1f53927c67d2b46910f
dorchard <dom.orchard@gmail.com>
2023-07-27 10:08:23 +0100

    more docs update

--- src/Control/Effect.hs
+++ src/Control/Effect.hs
@@ -1,12 +1,12 @@
 {-# LANGUAGE KindSignatures, TypeFamilies, ConstraintKinds, PolyKinds, MultiParamTypeClasses #-}
 
-module Control.Effect where 
+module Control.Effect where
 
 import Prelude hiding (Monad(..))
-import GHC.Exts ( Constraint )    
+import GHC.Exts ( Constraint )
 
-{-| Specifies "parametric effect monads" which are essentially monads but
-     annotated by a type-level monoid formed by 'Plus' and 'Unit' -}
+{-| Specifies "graded monads" which are indexed by
+     a type-level monoid formed by 'Plus' and 'Unit' -}
 class Effect (m :: k -> * -> *) where
 
    {-| Effect of a trivially effectful computation |-}
@@ -18,18 +18,18 @@ class Effect (m :: k -> * -> *) where
    type Inv m (f :: k) (g :: k) :: Constraint
    type Inv m f g = ()
 
-   {-| Effect-parameterised version of 'return'. Annotated with the 'Unit m' effect, 
+   {-| Graded version of 'return'. Annotated with the 'Unit m' effect,
     denoting pure compuation -}
    return :: a -> m (Unit m) a
 
-   {-| Effect-parameterise version of '>>=' (bind). Combines 
+   {-| Graded version of '>>=' (bind). Combines
     two effect annotations 'f' and 'g' on its parameter computations into 'Plus' -}
 
    (>>=) :: (Inv m f g) => m f a -> (a -> m g b) -> m (Plus m f g) b
 
    (>>) :: (Inv m f g) => m f a -> m g b -> m (Plus m f g) b
    x >> y = x >>= (\_ -> y)
-  
+
 fail = undefined
 
 {-| Specifies subeffecting behaviour -}
